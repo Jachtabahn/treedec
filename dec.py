daa6b3cfab3f46470a925271d204e756217f1a55
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-09-20 16:24:00 +0200

    Make tree game tree recursive

--- dec.py
+++ dec.py
@@ -5,243 +5,124 @@ from sys import argv
 
 logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
-STATUS_UNKNOWN = 0
-STATUS_FAILED = 1
-STATUS_SUCCESS = 2
+UNKNOWN = 0
+FAILED = 1
+SUCCESS = 2
 
-class SearchTree:
+class DecompositionNode:
 
-    def __init__(self, split_graph):
-        self.predecessor = {0: None}
-        self.subgraph = {0: split_graph}
+    def __init__(self, pred, labelled_subgraph, is_bag):
+        self.predecessor = pred
+        self.subgraph = labelled_subgraph
+        self.successors = None
+        self.is_bag = is_bag
+        self.strategy = None
+        self.status = UNKNOWN
 
-        self.successors = {0: None}
-        self.unprocessed_successors = {0: None}
-        self.is_bag = {0: True}
-        self.chosen_successor = {0: None}
-        self.status = {0: STATUS_UNKNOWN}
-        self.num_nodes = 1
+    def add_child(self, child):
+        self.successors.append(child)
 
-    def delete(self, node):
-        if node not in self.successors: return
-        deletable_successors = self.successors[node] if self.successors[node] is not None else []
-        for succ in deletable_successors:
-            self.delete(succ)
+    def set_status(self, new_status):
+        self.status = new_status
 
-        pred = self.predecessor[node]
-        self.successors[pred].remove(node)
-        if self.chosen_successor[pred] == node:
-            self.chosen_successor[pred] = None
-
-        del self.predecessor[node]
-        del self.subgraph[node]
-        del self.successors[node]
-        del self.unprocessed_successors[node]
-        del self.is_bag[node]
-        del self.chosen_successor[node]
-        del self.status[node]
-        self.num_nodes -= 1
-
-    def add(self, node, labelled_subgraph):
-        new_node = self.num_nodes
-        self.predecessor[new_node] = node
-        self.subgraph[new_node] = labelled_subgraph
-
-        self.successors[new_node] = None
-        self.unprocessed_successors[new_node] = None
-        self.is_bag[new_node] = not self.is_bag[node]
-        self.chosen_successor[new_node] = None
-        self.status[new_node] = STATUS_UNKNOWN
-
-        self.successors[node].append(new_node)
-        self.unprocessed_successors[node].append(new_node)
-        self.num_nodes += 1
-
-    def set_status(self, node, status):
-        self.status[node] = status
-
-    def get_predecessor(self, node):
-        return self.predecessor[node]
-
-    def mark_processed(self, pred, node):
-        self.unprocessed_successors[pred].remove(node)
-
-    def get_subgraph(self, node):
-        return self.subgraph[node]
-
-    def get_root(self):
-        return 0
-
-    def nodes(self):
-        return list(self.successors.keys())
-
-    def dot_string(self):
-        s = 'digraph {\n'
-        s += 'graph [compound=true, ranksep=1, nodesep=1]\n'
-        s += 'edge [penwidth=3]\n'
-        s += 'node [style=filled, color=aliceblue]\n'
-
-        edge_color = '#00ced172'
-        bag_color = '#ff8c00b2'
-        status_color = {
-            STATUS_UNKNOWN: 'gray',
-            STATUS_FAILED: 'crimson',
-            STATUS_SUCCESS: 'green3'
-        }
-        root_graph = self.subgraph[self.get_root()]
-        for node, node_subgraph in self.subgraph.items():
-            is_bag_node = self.is_bag[node]
-            color = bag_color if is_bag_node else edge_color
-            node_name = f'b{node}' if is_bag_node else f'e{node}'
-            node_label = f'Bag {node}' if is_bag_node else f'Edge {node}'
-
-            s += f'subgraph cluster_{node_name} '
-            s += '{\n'
-            s += f'graph [label="{node_label}", style=rounded, '
-            node_status = self.status[node]
-            s += f'bgcolor="{color}", penwidth=8, color={status_color[node_status]}]\n'
-            s += 'edge [penwidth=1, dir=none]\n'
-            s += f'{node_name} [style=invis]\n'
-            subgraph_string = node_subgraph.dot_string(node_name, root_graph)
-            s += subgraph_string
-            s += '}\n'
-
-        chosen_color = 'red'
-        for node, pred in self.predecessor.items():
-            if pred is None: continue
-
-            node_name = f'b{node}' if self.is_bag[node] else f'e{node}'
-            pred_name = f'b{pred}' if self.is_bag[pred] else f'e{pred}'
-            color = f', color={chosen_color}' if self.chosen_successor[pred] == node else ''
-
-            s += f'{pred_name} -> {node_name} [ltail=cluster_{pred_name}, '
-            s += f'lhead=cluster_{node_name}{color}]\n'
-
-        s += '}'
-        return s
-
-    def edges_string(self):
-        s = ''
-        for node in self.nodes():
-            pred = self.predecessor[node]
-            if pred is None: continue
-            pred_type = 'Bag' if self.is_bag[pred] else 'Edge'
-            node_type = 'Bag' if self.is_bag[node] else 'Edge'
-            s += f'{pred_type} {pred} -> {node_type} {node}\n'
-        return s
-
-    def __str__(self):
-        s = ''
-        for node in self.nodes():
-            node_type = 'Bag' if self.is_bag[node] else 'Edge'
-            s += f'The status at {node_type} {node} is {self.status[node]}\n'
-            s += f'The chosen successor at {node_type} {node} is {self.chosen_successor[node]}\n'
-            s += f'The subgraph at {node_type} {node} is\n{self.subgraph[node]}'
-        return s
 '''
     Returns a pair of the search tree and a boolean indicating whether the search tree contains a valid
     tree decomposition.
 '''
 def compute_tree_decomposition(split_graph, maximum_bag_size):
-    search_tree = SearchTree(split_graph)
-    node = search_tree.get_root()
+    node = DecompositionNode(pred=None, labelled_subgraph=split_graph, is_bag=True)
     while 1:
-        if search_tree.is_bag[node]:
-            # add all required successor bag edges
-            if search_tree.successors[node] is None:
-                search_tree.successors[node] = []
-                search_tree.unprocessed_successors[node] = []
+        if node.is_bag:
+            bag = node
 
-                node_subgraph = search_tree.get_subgraph(node)
-                components = decompose_into_connected_components(node_subgraph)
+            # add all required successor bag edges
+            if bag.successors is None:
+                bag.successors = []
+
+                components = decompose_into_connected_components(bag.subgraph)
                 for comp in components:
-                    search_tree.add(node, comp)
+                    child = DecompositionNode(pred=bag, labelled_subgraph=comp, is_bag=False)
+                    bag.add_child(child)
 
             # quit on failure because we need every component to work out
-            if search_tree.status[node] == STATUS_FAILED:
-                pred = search_tree.get_predecessor(node)
-                if pred is None: return search_tree, False
-                search_tree.mark_processed(pred, node)
-                node = pred
+            for edge in bag.successors:
+                if edge.status == FAILED:
+                    bag.status = FAILED
+                    bag.strategy = edge
+                    break
+
+            if bag.status == FAILED:
+                if bag.predecessor is None: return bag, False
+                node = bag.predecessor
                 continue
 
-            # choose some unworked successor
-            unknown_bag_edges = [succ for succ in search_tree.unprocessed_successors[node] \
-                if search_tree.status[succ] == STATUS_UNKNOWN]
-            if not unknown_bag_edges:
-                search_tree.set_status(node, STATUS_SUCCESS)
-                pred = search_tree.get_predecessor(node)
-                if pred is None: return search_tree, True
-                search_tree.mark_processed(pred, node)
-                search_tree.set_status(pred, STATUS_SUCCESS)
-                search_tree.chosen_successor[pred] = node
-                node = pred
-            else:
-                unknown_subgraphs = [search_tree.get_subgraph(succ) for succ in unknown_bag_edges]
+            unknown_bag_edges = [edge for edge in bag.successors if edge.status == UNKNOWN]
+            if unknown_bag_edges:
+                unknown_subgraphs = [edge.subgraph for edge in unknown_bag_edges]
                 index = choose_weakest_component(unknown_subgraphs)
                 node = unknown_bag_edges[index]
+            else:
+                bag.set_status(SUCCESS)
+                if bag.predecessor is None: return bag, True
+                node = bag.predecessor
         else:
-            # add all possible successor bags
-            if search_tree.successors[node] is None:
-                search_tree.successors[node] = []
-                search_tree.unprocessed_successors[node] = []
+            edge = node
 
-                escape_component = search_tree.get_subgraph(node)
-                choosable_cops = compute_choosable_cops(escape_component, maximum_bag_size)
-                for cop in choosable_cops:
-                    new_subgraph = escape_component.copy()
-                    new_subgraph.place(cop)
-                    search_tree.add(node, new_subgraph)
+            if edge.successors is None:
+                edge.successors = []
 
             # quit on success because we only need some cop to work
-            if search_tree.status[node] == STATUS_SUCCESS:
-                pred = search_tree.get_predecessor(node)
-                if pred is None: return search_tree, True
-                search_tree.mark_processed(pred, node)
-                node = pred
+            for bag in edge.successors:
+                if bag.status == SUCCESS:
+                    edge.status = SUCCESS
+                    edge.strategy = bag
+                    break
+
+            if edge.status == SUCCESS:
+                # the predecessor of an edge will never be None because the single root is a bag
+                node = edge.predecessor
                 continue
 
-            # choose some unworked successor
-            unknown_bags = [succ for succ in search_tree.unprocessed_successors[node] \
-                if search_tree.status[succ] == STATUS_UNKNOWN]
-            if not unknown_bags:
-                pred = search_tree.get_predecessor(node)
-                assert pred is not None # because the root is always a bag
+            known_cops = [bag.subgraph.new_cop for bag in edge.successors]
+            choosable_cops = compute_choosable_cops(edge.subgraph, known_cops, maximum_bag_size)
 
-                # clear memory
-                for failed_edge in search_tree.successors[pred]:
-                    search_tree.delete(failed_edge)
+            if choosable_cops:
+                index = choose_strongest_cop(edge.subgraph, choosable_cops)
 
-                search_tree.set_status(node, STATUS_FAILED)
-                search_tree.mark_processed(pred, node)
-                search_tree.set_status(pred, STATUS_FAILED)
-                search_tree.chosen_successor[pred] = node
-                node = pred
+                # create a new bag on the fly
+                cop = choosable_cops[index]
+                split_subgraph = edge.subgraph.copy()
+                split_subgraph.place(cop)
+                child = DecompositionNode(pred=edge, labelled_subgraph=split_subgraph, is_bag=True)
+                edge.add_child(child)
+
+                node = child
             else:
-                unknown_cops = [search_tree.get_subgraph(succ).new_cop for succ in unknown_bags]
-                index = choose_strongest_cop(search_tree.get_subgraph(node), unknown_cops)
-                node = unknown_bags[index]
+                edge.set_status(FAILED)
+                node = edge.predecessor
     return None
 
 '''
+    Choose a component that is most likely to fail to decompose with the given maximum bag size
     The given list of labelled subgraphs is not empty.
 '''
 def choose_weakest_component(unknown_subgraphs):
     return len(unknown_subgraphs)-1
 
 '''
-    The given list of labelled subgraphs is not empty.
+    Choose a cop that is most likely to succeed to decompose with the given maximum bag size
+    The given list of choosable cops is not empty.
 '''
 def choose_strongest_cop(subgraph, unknown_cops):
     return 0
 
-def compute_choosable_cops(escape_component, maximum_bag_size):
+def compute_choosable_cops(escape_component, known_cops, maximum_bag_size):
     choosable = []
     if len(escape_component.cops) >= maximum_bag_size:
         return choosable
-    for node in escape_component.nodes():
-        if not escape_component.is_cop(node):
-            choosable.append(node)
+    for vertex in escape_component.adjacent:
+        if not escape_component.is_cop(vertex) and vertex not in known_cops:
+            choosable.append(vertex)
     return choosable
 
 def parse_graph(filepath):
@@ -269,11 +150,11 @@ if __name__ == '__main__':
     search_tree, success = compute_tree_decomposition(G, treewidth+1)
     if success:
         logging.info('Successfully computed a tree decomposition.')
-        logging.debug(search_tree.edges_string())
-        logging.debug(str(search_tree))
+        # logging.debug(search_tree.edges_string())
+        # logging.debug(str(search_tree))
     else:
         logging.error('Failed computing a tree decomposition.')
 
-    tree_dot_string = search_tree.dot_string()
-    with open('generated.dot', 'w') as f:
-        print(tree_dot_string, file=f)
+    # tree_dot_string = search_tree.dot_string()
+    # with open('generated.dot', 'w') as f:
+    #     print(tree_dot_string, file=f)
