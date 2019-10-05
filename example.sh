30a0904ab77975a68e6388c018e129f5b87a12b1
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-10-05 22:02:29 +0200

    Delete some old stuff

--- example.sh
+++
@@ -1,7 +0,0 @@
-!#/bin/bash
-
-dot -Tsvg dot/bag1.dot -o svg/bag1.svg
-dot -Tsvg dot/edge2.dot -o svg/edge2.svg
-dot -Tsvg dot/edge3.dot -o svg/edge3.svg
-
-dot -Tsvg dot/forest.dot -o svg/forest.svg
