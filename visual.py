e5e4edffe8fdcc82b555a4de1263e5e323b275c1
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2019-10-20 03:39:35 +0200

    visual: Edit script to visualize all the structures in the networks/ directory

--- visual.py
+++ visual.py
@@ -5,26 +5,70 @@ import argparse
 import os
 from os import path
 
-def determine_type(file):
-    stdin_lines = file.read().split('\n')[:-1]
-    for line in stdin_lines:
-        if line[0] == 'c':
-            continue
-        if line[0] == 'p':
-            return stdin_lines, 'network'
-        if line[0] == 's':
-            return stdin_lines, 'treedec'
-    return stdin_lines, None
+def normpath(my_path):
+    if my_path is None: return None
+    return path.normpath(my_path)
+
+def exists(my_path):
+    if my_path is None: return False
+    return path.exists(my_path)
+
+def visualize_structures(networks_dir, structs_dir):
+    if networks_dir is None:
+        structure_paths = [structs_dir]
+    else:
+        structure_paths = []
+        for network_dir in os.listdir(networks_dir):
+            structure_path = path.join(networks_dir, network_dir)
+            structure_paths.append(structure_path)
+
+    for structure_path in structure_paths:
+        logging.info(f'Visualizing structures in {structure_path}')
+        for filename in os.listdir(structure_path):
+            filepath = path.join(structs_dir, filename)
+            structure_name = filename[:-3]
+
+            if filename[-3:] == '.gr':
+                with open(filepath) as struct_file:
+                    structure = network.parse(struct_file)
+
+                # create a dot string from the parsed structure
+                dot_string = 'info.dot = `\n'
+                dot_string += structure.visualize()
+                dot_string += '`\n'
+            elif filename[-3:] == '.td':
+                with open(filepath) as struct_file:
+                    structure = treedec.parse(struct_file)
+
+                # create a dot string from the parsed structure
+                dot_string = f'if ("{structure_name}" in info.treedecs) '
+                dot_string += '{ '
+                dot_string += f'info.treedecs["{structure_name}"].dot = `\n'
+                dot_string += structure.visualize()
+                dot_string += '`}\n'
+            else:
+                logging.warning(f'Ignoring file {filename} with unknown extension.')
+                continue
+
+            # write that dot string to a file
+            network_dir, _ = structs_dir.split()
+            visual_path = path.join(network_dir, 'visuals')
+            if path.exists(visual_path):
+                logging.warning(f'Overwriting file {visual_path}..')
+            else:
+                logging.info(f'Creating new file {visual_path}..')
+            with open(visual_path, 'w') as visual_file:
+                visual_file.write(dot_string)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--structs-dir', '-s', required=True, type=str)
-    parser.add_argument('--visuals-dir', '-d', required=True, type=str)
+    parser.add_argument('--networks-dir', '-n', type=str)
+    parser.add_argument('--structs-dir', '-s', type=str)
     parser.add_argument('--verbose', '-v', action='count')
     args = parser.parse_args()
 
-    args.structs_dir = path.normpath(args.structs_dir)
-    args.visuals_dir = path.normpath(args.visuals_dir)
+    args.networks_dir = normpath(args.networks_dir)
+    args.structs_dir = normpath(args.structs_dir)
 
     log_levels = {
         None: logging.WARNING,
@@ -35,37 +79,13 @@ if __name__ == '__main__':
         args.verbose = len(log_levels)-1
     logging.basicConfig(format='%(message)s', level=log_levels[args.verbose])
 
-    for filename in os.listdir(args.structs_dir):
-        filepath = f'{args.structs_dir}/{filename}'
-        structure_name = filename[:-3]
+    if exists(args.networks_dir):
+        logging.info(f'Visualization whole networks directory {args.networks_dir}')
+    elif exists(args.structs_dir) and exists(args.structs_dir):
+        logging.info(f'Visualizing specific network at {args.structs_dir}')
+        args.networks_dir = None
+    else:
+        logging.error(f'At least one of --networks, or --structs_dir and --visuals_dir is required!')
+        exit(1)
 
-        if filename[-3:] == '.gr':
-            with open(filepath) as struct_file:
-                structure = network.parse(struct_file)
-
-            # create a dot string from the parsed structure
-            dot_string = 'info.dot = `\n'
-            dot_string += structure.visualize()
-            dot_string += '`\n'
-        elif filename[-3:] == '.td':
-            with open(filepath) as struct_file:
-                structure = treedec.parse(struct_file)
-
-            # create a dot string from the parsed structure
-            dot_string = f'if ("{structure_name}" in info.treedecs) '
-            dot_string += '{ '
-            dot_string += f'info.treedecs["{structure_name}"].dot = `\n'
-            dot_string += structure.visualize()
-            dot_string += '`}\n'
-        else:
-            logging.warning(f'Ignoring file {filename} with unknown extension.')
-            continue
-
-        # write that dot string to a file
-        visual_path = f'{args.visuals_dir}/{structure_name}.dot.js'
-        if path.exists(visual_path):
-            logging.warning(f'Overwriting file {visual_path}..')
-        else:
-            logging.info(f'Creating new file {visual_path}..')
-        with open(visual_path, 'w') as visual_file:
-            visual_file.write(dot_string)
+    visualize_structures(args.networks_dir, args.structs_dir)
