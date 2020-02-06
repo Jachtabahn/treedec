38e67570595be9423cf7fbaf24cb90d372aee8d7
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2020-02-06 13:35:40 +0100

    Add a script to convert all .td files to .graphml

---
+++ convert_all_graphml.py
@@ -0,0 +1,9 @@
+import td2graphml
+
+with open("/home/habimm/treedec/csv/habimm-decomposed-networks.csv") as networks_file:
+    network_names = networks_file.read().split()
+print(network_names)
+
+for network_name in network_names:
+    subproc
+    python ~/convert/td2graphml.py < /var/www/html/networks/fuzix_devio_kprintf/structs/luebeck.td > /var/www/html/networks/fuzix_devio_kprintf/structs/luebeck.graphml
