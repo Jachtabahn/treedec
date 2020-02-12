e5119cd9a9eb986bfc3f04369c8028329a611380
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2020-02-12 15:13:48 +0100

    Display columns with three segments each for every decomposer

--- bars.py
+++ bars.py
@@ -8,59 +8,57 @@ import sqlite3
 connection = sqlite3.connect("/home/habimm/treedec/data/data.db")
 cursor = connection.cursor()
 cursor.execute("""
-  SELECT input_id, AVG(milliseconds), COUNT(*), network_name, network_id, formula_name, decomposer_name
-    FROM sequoia_runtimes JOIN sequoia_inputs ON
-    sequoia_runtimes.input_id == sequoia_inputs.rowid GROUP BY input_id
+  SELECT formula_name, networks.rowid, decomposer_name, AVG(milliseconds)
+    FROM sequoia_runtimes JOIN sequoia_inputs JOIN networks ON
+    sequoia_runtimes.input_id == sequoia_inputs.rowid AND networks.name == sequoia_inputs.network_name GROUP BY input_id
 """)
 inputs = cursor.fetchall()
+inputs = [input for input in inputs if input[0] == "longest-cycle"]
 connection.close()
 
-sequoia_problems = {(input[3], input[4]): [] for input in inputs}
-for input in inputs:
-  sequoia_problems[(input[3], input[4])].append(input)
+network_ids = list(set(row[1] for row in inputs))
+sequoia = {}
+meiji2016 = {}
+habimm = {}
+for formula_name, network_id, decomposer_name, milliseconds in inputs:
+  if decomposer_name == "sequoia":
+    sequoia[network_id] = milliseconds
+  elif decomposer_name == "meiji2016":
+    meiji2016[network_id] = milliseconds
+  elif decomposer_name == "habimm":
+    habimm[network_id] = milliseconds
 
-for problem, problem_inputs in list(sequoia_problems.items()):
-  if len(problem_inputs) != 3:
-    del sequoia_problems[problem]
-
-inputs_list = list(sequoia_problems.values())
-inputs = sum(inputs_list, [])
-
-inputs = [input for input in inputs if input[4] == "hamiltonian-cycle"]
-
-# inputs.sort(key = lambda row: row[1])
+network_ids.sort(key = lambda network_id: sequoia[network_id] + meiji2016[network_id] + habimm[network_id] if network_id in meiji2016 else sequoia[network_id] + habimm[network_id])
 
 # Convert the fetched rows to a dictionary with a key for each column.
-decomposer_color = {
-  "": "red",
-  "habimm": "green",
-  "meiji2016": "blue"
-}
 runtimes_table = {
-  "input_id": [str(input_id) for input_id, _, _, _, _, _ in inputs],
-  "milliseconds": [milliseconds for _, milliseconds, _, _, _, _ in inputs],
-  "color": [decomposer_color[decomposer_name] for _, _, _, _, _, decomposer_name in inputs]
+  "network_id": [str(network_id) for network_id in network_ids],
+  "sequoia": [sequoia[network_id] for network_id in network_ids],
+  "meiji2016": [meiji2016[network_id] if network_id in meiji2016 else None for network_id in network_ids],
+  "habimm": [habimm[network_id] for network_id in network_ids]
 }
 
 # Create the bar chart with the Sequoia input IDs and their runtimes.
 plot = bokeh.plotting.figure(
-  title="Sequoia runtimes",
-  x_range=runtimes_table["input_id"],
+  title="Sequoia Runtimes",
+  x_range=runtimes_table["network_id"],
   plot_height=1080,
   plot_width=1920,
   tools="box_zoom,reset,hover",
   toolbar_location=None,
   tooltips=[
-    ("Input ID", "@input_id"),
-    ("Milliseconds", "@milliseconds")
-  ]
+    ("Habimm", "@habimm{1} ms"),
+    ("Meiji 2016", "@meiji2016{1} ms"),
+    ("Sequoia", "@sequoia{1} ms"),
+    ("Input ID", "@network_id")]
 )
-plot.vbar(
-  x="input_id",
-  top="milliseconds",
-  color="color",
+plot.vbar_stack(
+  ["sequoia", "meiji2016", "habimm"],
+  x="network_id",
   width=0.9,
-  source=bokeh.plotting.ColumnDataSource(runtimes_table))
+  color=["#c9d9d3", "#718dbf", "#e84d60"],
+  source=runtimes_table
+)
 plot.yaxis.axis_label = "Runtime in milliseconds"
 plot.xaxis.axis_label = "Input IDs"
 plot.xaxis.major_tick_line_width = 0
